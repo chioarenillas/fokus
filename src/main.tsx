64466b2b7a56241a0bac91cde57aa2eb95ee558a
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2026-05-08 12:46:12 +0200

    new

--- src/main.tsx
+++ src/main.tsx
@@ -7,10 +7,8 @@ import { AuthProvider } from './context/AuthContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
     <BrowserRouter>
       <App/>
     </BrowserRouter>
-    </AuthProvider>
   </StrictMode>,
 )
