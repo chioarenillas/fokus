a5e05c684904f7510ce4e9fbb850622250f234b2
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2026-06-10 16:53:26 +0200

    new toggle tests

--- src/components/Hooks/useTheme.test.tsx
+++ src/components/Hooks/useTheme.test.tsx
@@ -1,7 +1,6 @@
-import {  renderHook } from "@testing-library/react"
+import {  renderHook, act } from "@testing-library/react"
 import { describe, it, expect, vi } from "vitest"
 import { useTheme } from "./useTheme"
-
 
 describe("useTheme", () => {
 
@@ -19,6 +18,26 @@ describe("useTheme", () => {
         }))
         const { result } = renderHook(() => useTheme())
         expect(result.current.theme).toBe("dark")
+    })
+
+    it("toggleTheme changes from ligth to dark", () => {
+        vi.spyOn(Storage.prototype, "getItem").mockReturnValue("light")
+        const {result } = renderHook(() => useTheme())
+
+        act(() => {
+            result.current.toggleTheme()
+        }) 
+        expect(result.current.theme).toBe("dark")
+    })
+
+    it("toggleTheme changes from dark to light", () => {
+        vi.spyOn(Storage.prototype, "getItem").mockReturnValue("dark")
+        const { result } = renderHook(() => useTheme())
+
+        act(() => {
+            result.current.toggleTheme()
+        })
+        expect(result.current.theme).toBe("light")
     })
 
 })
